7e5afda6ad03ccb6822873c0dd23f075cb5725e6
iamstephan <stephanburger54@gmail.com>
2021-10-07 12:07:52 +0200

    controls now working

--- src/scripts/components/cred_carousel.ts
+++ src/scripts/components/cred_carousel.ts
@@ -1,12 +1,24 @@
 function InitCredCarousel() {
   const carouselNode = document.querySelector<HTMLDivElement>(".cred-wrapper");
-
   const options = {
     loop: true,
   };
-
-  console.log("hi");
   const Embla = window.EmblaCarousel(carouselNode, options);
+
+  const prevButton = document.querySelector<HTMLButtonElement>(
+    ".cred-section .prev-control"
+  );
+  const nextButton = document.querySelector<HTMLButtonElement>(
+    ".cred-section .next-control"
+  );
+
+  prevButton.addEventListener("click", () => {
+    Embla.scrollPrev();
+  });
+
+  nextButton.addEventListener("click", () => {
+    Embla.scrollNext();
+  });
 }
 
 try {
