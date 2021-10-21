d30038dce634ee78e58dd030ee72fb7ec299058f
iamstephan <stephanburger54@gmail.com>
2021-10-21 12:58:25 +0200

    import new script on both page types

--- src/scripts/_page_imports/content_template.ts
+++ src/scripts/_page_imports/content_template.ts
@@ -1,13 +1,13 @@
-import "../components/accordion";
-import "../components/silo";
 import "../components/sticky_nav";
 import "../components/supernav";
+
+import "../components/accordion";
+import "../components/silo";
 import "../components/lightbox";
 import "../components/related_select";
 import "../components/colorlink";
+import "../components/transitions";
+import "../components/video_logo";
 
-// Macros
 import "../macros/questions_answers";
 import "../macros/testimonials";
-
-import "../components/transitions";
--- src/scripts/_page_imports/home.ts
+++ src/scripts/_page_imports/home.ts
@@ -1,5 +1,6 @@
 import "../components/sticky_nav";
 import "../components/supernav";
-import "../components/cred_carousel";
 
+import "../components/cred_carousel";
 import "../components/transitions";
+import "../components/video_logo";
